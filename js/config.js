6f2de4e832675607140e01c14b5f88c0bcfa4e38
wahyu <wahyudiva2004@gmail.com>
2025-01-31 22:33:18 +0800

    UPDATE - hilangkan api key di config

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
     HUGGING_FACE_API_KEY: typeof process !== 'undefined' && process.env ? 
         process.env.HUGGING_FACE_API_KEY : 
-        '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' // Fallback untuk development
+        '' // Fallback untuk development
 };
